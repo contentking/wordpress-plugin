79f09fb8e8942161baec9e63534e38311d37ef85
Robert Jurinek <jurinek@vitixcompany.cz>
2017-06-23 21:05:32 +0200

    Post types test and bulk edit support

--- lib/contentking-api.php
+++ lib/contentking-api.php
@@ -3,7 +3,7 @@
 class ContentkingAPI implements ContentkingAPIInterface {
 
 	protected $api_url = 'https://api.contentkingapp.com/v1/';
-	//protected $api_url = 'https://staging-api.contentking.io/v1/';
+
 	/*
 	* Performs api call to validate token.
 	*
@@ -38,8 +38,7 @@ class ContentkingAPI implements ContentkingAPIInterface {
 
 		$data = $this->prepare_request_data( ['url'=> $url] );
 		$response = wp_remote_post( $this->api_url . 'check_url', $data );
-		$logger = new LoggerFile();
-		$logger->log(print_r($response, true));
+	
 		if ( !is_wp_error( $response ) ):
 			if( isset( $response['response']['code'] ) ):
 				if(intval( $response['response']['code'] ) === 200):

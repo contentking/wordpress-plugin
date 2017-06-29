f2a4a3b2724681756a791373bb06fed9bd85865a
Robert Jurinek <jurinek@vitixcompany.cz>
2017-06-29 15:21:50 +0200

    Fix urls for posts with trashed parent

--- lib/contentking-save-post.php
+++ lib/contentking-save-post.php
@@ -33,8 +33,9 @@ class ContentkingSavePost extends WP_Async_Task{
 
 			$post_type_data = get_post_type_object( $data[1]->post_type );
 			if( intval( $post_type_data->public ) === 1 || intval( $post_type_data->publicly_queryable ) === 1 ): //Post has public URL
-
-				array_push( $this->urls, get_permalink( $data[0] ) ); //Only data from last call will be used in async task
+				$url = get_permalink( $data[0] );
+				$fixed_url = str_replace( '__trashed', '', $url); //Fix url in case parent page was thrashed recently.
+				array_push( $this->urls, $fixed_url ); //Only data from last call will be used in async task
 
 				return [
 					'urls' => $this->urls,

bed6818d81e1156c836301901926bcb1c1d405d4
Robert Jurinek <jurinek@vitixcompany.cz>
2017-07-24 16:18:40 +0200

    0.6.1 Fix contributors

--- wp-contentking.php
+++ wp-contentking.php
@@ -6,7 +6,7 @@
  * Author URI:      https://www.contentkingapp.com/
  * Text Domain:     contentking-plugin
  * Domain Path:
- * Version:         0.6.0
+ * Version:         0.6.1
  *
  * @package         contentking-plugin
  */

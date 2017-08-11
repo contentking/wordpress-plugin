e1c0367d8263788c2c36b4a2f65ed9c110c2f453
Angelika <dngerousminds@gmail.com>
2017-08-11 16:58:49 +0200

    1.4.0 send yoast xml sitemap on change

---
+++ lib/contentking-sitemap-change.php
@@ -0,0 +1,53 @@
+<?php
+
+class ContentkingChangeSitemap extends WP_Async_Task{
+
+	/**
+	* Action to use to trigger this task
+	*
+	* @var string
+	*/
+	protected $action = 'contentking_updated_sitemap'; //Action, which will react, if there're any changes with sitemap
+	/**
+	 * Priority to fire intermediate action.
+	 *
+	 * @var int
+	 */
+	protected $priority = 9999; //We need this to have pretty high to make sure all other actions are done.
+
+	/**
+	* Prepare POST data to send to session that processes the task
+	*
+	* @param array $data Params from hook
+	*
+	* @return array
+	*/
+	protected function prepare_data($data){
+
+		if ( class_exists( 'WPSEO_Sitemaps_Router' ) ):
+
+			$url = WPSEO_Sitemaps_Router::get_base_url( 'sitemap_index.xml' );
+					
+			return [
+				'wpseo_xml' => [$url],
+			];
+
+		endif;
+
+		return null;
+	}
+
+	/**
+	* Run the asynchronous task
+	*
+	* Calls all functions hooked to async hook
+	*/
+	protected function run_action() {
+
+		if( isset( $_POST['wpseo_xml'] ) ):
+			do_action( "wp_async_$this->action", $_POST['wpseo_xml'] );
+		endif;
+
+	}
+
+}

060bcb5edc5a0b84d43bb9e9210aa4c0d6501fe9
Robert Jurinek <jurinek@vitixcompany.cz>
2017-11-13 15:29:33 +0100

    Coding standards for settings page

--- app/screens/settings.php
+++ app/screens/settings.php
@@ -8,12 +8,12 @@
 defined( 'ABSPATH' ) || die( 'This file should not be accessed directly!' );
 
 if ( ! current_user_can( 'manage_options' ) ) :
-	wp_die( __( 'You do not have sufficient permissions to access this page.', 'contentking-plugin' ) );
+	wp_die( esc_html__( 'You do not have sufficient permissions to access this page.', 'contentking-plugin' ) );
 endif;
 
 ?>
 
-<h1> <?php _e( 'ContentKing settings', 'contentking-plugin' ); ?></h1>
+<h1> <?php echo esc_html( __( 'ContentKing settings', 'contentking-plugin' ) ); ?></h1>
 <form action="options.php" method="post">
 
 <?php
@@ -23,8 +23,8 @@ endif;
 
 	<table class = "contentking-setting">
 		<tr>
-			<th scope="row"> <?php _e( 'API token', 'contentking-plugin' ); ?> </th>
-			<td><input type="text" id="contentking_client_token" name="contentking_client_token" value="<?php echo $contentking_client_token; ?>" />	</td>
+			<th scope="row"> <?php echo esc_html__( 'API token', 'contentking-plugin' ); ?> </th>
+			<td><input type="text" id="contentking_client_token" name="contentking_client_token" value="<?php echo esc_attr( $contentking_client_token ); ?>" />	</td>
 			<td>
 				<?php
 
@@ -46,37 +46,67 @@ endif;
 
 	<input type="hidden" name="update_contentking_token" value="1"/>
 	<br/>
-	<input name="Submit" type="submit" class="button button-primary" value="<?php _e( 'Save changes', 'contentking-plugin' ); ?>" />
+	<input name="Submit" type="submit" class="button button-primary" value="<?php echo esc_attr__( 'Save changes', 'contentking-plugin' ); ?>" />
 </form>
 <br/>
 <?php
-if ( strlen( $contentking_client_token ) > 5 && $flag === '0' ) :
+if ( 5 < strlen( $contentking_client_token ) && '0' === $flag ) :
 	?>
 	<form method="post">
 		<?php wp_nonce_field( 'contentking_validate_token', 'ck_validate_token' ); ?>
 	<input type="hidden" name="validate_contentking_token" value="1"/>
-	<input name="Validate" type="submit" class="button button-primary" value="<?php _e( 'Validate token', 'contentking-plugin' ); ?>" />
+	<input name="Validate" type="submit" class="button button-primary" value="<?php echo esc_attr__( 'Validate token', 'contentking-plugin' ); ?>" />
 	<p class="error">
-	<?php _e( 'It looks like your API token is not validated. Click to validate.','contentking-plugin' ); ?>
+	<?php esc_html__( 'It looks like your API token is not validated. Click to validate.','contentking-plugin' ); ?>
 	</p>
 	</form>
 
 <?php
 	endif;
 	?>
-<h2> <?php _e( 'Instructions', 'contentking-plugin' ); ?></h2>
+<h2> <?php echo esc_html__( 'Instructions', 'contentking-plugin' ); ?></h2>
 <ol>
-	<li><?php _e( 'In a new window, log in to the ContentKing app.','contentking-plugin' ); ?></li>
-	<li><?php _e( 'Go to <strong>Account</strong> then <strong>Team profile</strong>.','contentking-plugin' ); ?></li>
-	<li><?php _e( 'Copy the API token from the <strong>Integrations</strong> section.','contentking-plugin' ); ?></li>
-	<li><?php _e( 'Come back to this WordPress screen and paste the token into the <strong>API token</strong> field.','contentking-plugin' ); ?></li>
-	<li><?php _e( 'Press <strong>Save changes</strong> and once you see the green validation tick mark, you\'re good to go.','contentking-plugin' ); ?></li>
+	<li>
+	<?php
+	_e( // WPCS: XSS OK.
+		'In a new window, log in to the ContentKing app.','contentking-plugin'
+	);
+			?>
+		 </li>
+	<li>
+	<?php
+	_e( // WPCS: XSS OK.
+		'Go to <strong>Account</strong> then <strong>Team profile</strong>.','contentking-plugin'
+	);
+			?>
+		 </li>
+	<li>
+	<?php
+	_e( // WPCS: XSS OK.
+		'Copy the API token from the <strong>Integrations</strong> section.','contentking-plugin'
+	);
+			?>
+		 </li>
+	<li>
+	<?php
+	_e( // WPCS: XSS OK.
+		'Come back to this WordPress screen and paste the token into the <strong>API token</strong> field.','contentking-plugin'
+	);
+			?>
+		 </li>
+	<li>
+	<?php
+	_e( // WPCS: XSS OK.
+		'Press <strong>Save changes</strong> and once you see the green validation tick mark, you\'re good to go.','contentking-plugin'
+	);
+			?>
+		 </li>
 </ol>
 <p>
 <?php
-_e(
+_e( // WPCS: XSS OK.
 	'The ContentKing plugin is active when you see its name in green in the upper WordPress bar.<br/>
-As always, our Support team is ready to help if you have any trouble.'
+As always, our Support team is ready to help if you have any trouble.', 'contentking-plugin'
 );
 ?>
 </p>
